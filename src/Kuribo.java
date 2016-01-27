11c896717bddc5cfaf48f2a3165d5c146cebec3a
尾崎周也 <Shuya@243.128.139.178.203.in-addr.arpa>
2016-01-27 15:31:55 +0900

    いらないimportを消した

--- src/Kuribo.java
+++ src/Kuribo.java
@@ -1,14 +1,4 @@
 import java.applet.*;
-import java.awt.event.*;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.awt.Rectangle;
-import java.io.IOException;
-import java.util.*;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
 
 public class Kuribo extends MovableGameObject {
   private static final int SPEED = 4; //スピード
