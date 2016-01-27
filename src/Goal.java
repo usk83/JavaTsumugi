0e69c0a437cdcee5cbfea9567a4929403befe843
Yasunobu Toyota <yas-nyan@sfc.wide.ad.jp>
2016-01-27 20:36:53 +0900

    ゴールクラスを作成。

---
+++ src/Goal.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Goal extends GameObject {
+
+    public Goal(float _px, float _py) {
+        super(_px, _py, "res/object/flag.png", 4);
+    }
+    
+   
+}
